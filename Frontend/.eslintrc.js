c3302a62d0ab549b7ba3311328f6f3a00cfadf87
inaba <software@panduro.guru>
2020-09-22 22:39:46 +0200

    Frontend init

---
+++ Frontend/.eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "@vue/standard",
+    "@vue/typescript/recommended"
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    semi: ["error", "always"],
+    "no-trailing-spaces": 0,
+    "comma-dangle": "error",
+    "no-console": "off",
+    quotes: [
+      "error",
+      "double",
+      { avoidEscape: true, allowTemplateLiterals: true }
+    ],
+    strictPropertyInitialization: 0,
+    "import/prefer-default-export": "off",
+    "import/no-default-export": "off",
+    "no-param-reassign": "off",
+    "class-methods-use-this": "off",
+    "lines-between-class-members": "off",
+    "max-len": 0,
+    "no-empty-function": 1,
+    "@typescript-eslint/no-empty-function": 1,
+    curly: 0,
+    "arrow-parens": 0,
+    "no-restricted-syntax": 1,
+    "nonblock-statement-body-position": 0,
+    "operator-linebreak": 0
+  },
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+      ],
+      env: {
+        mocha: true
+      }
+    }
+  ]
+};
